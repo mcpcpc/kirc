bafdaa2c26feb748fea037be586db803ddd1900b
mcpcpc <michaelczigler@icloud.com>
2020-10-09 20:03:09 -0400

    add check for out-of-bound read

--- kirc.c
+++ kirc.c
@@ -536,10 +536,12 @@ static int handleServerMessage(void) {
 }
 
 static void handleUserInput(char *usrin) {
+    if (usrin == NULL) return;
+
     char *tok;
     size_t msg_len = strlen(usrin);
 
-    if (usrin[msg_len - 1] == '\n') {
+    if (msg_len > 0 && usrin[msg_len - 1] == '\n') {
         usrin[msg_len - 1] = '\0';
     }
 
