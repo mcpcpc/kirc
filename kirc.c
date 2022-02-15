4d6aa92d1ce25ee76b9deb8116cca882ef9f2922
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2022-02-15 09:53:51 -0500

    bump version

--- kirc.c
+++ kirc.c
@@ -18,7 +18,7 @@
 #include <sys/ioctl.h>
 
 #define CTCP_CMDS "ACTION VERSION TIME CLIENTINFO PING"
-#define VERSION "0.3.0"
+#define VERSION "0.3.1"
 #define MSG_MAX 512
 #define CHA_MAX 200
 #define NIC_MAX 26
