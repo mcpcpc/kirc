26c7c199e96522f774a9319f3bf82992bd9237f5
stefan11111 <stefanalexe48@gmail.com>
2023-01-10 11:11:50 +0000

    Merge from master
    Fixed issue where you couldn't send messages to the channel if you started kirk without -c option.
    Added error messages to /join and /part commands.

--- kirc.c
+++ kirc.c
@@ -999,27 +999,61 @@ static void handle_user_input(state l)
     printf("\r\x1b[0K");
     switch (l->buf[0]) {
     case '/':			/* send system command */
-	if(l->buf[1]=='/'){
-                raw("privmsg #%s :%s\r\n", l->prompt, l->buf + 3);
-                printf("\x1b[35mprivmsg #%s :%s\x1b[0m\r\n", l->prompt, l->buf + 3);
-	}
-	else
-	if(!strncmp(l->buf+1, "MSG", 3)||!strncmp(l->buf+1, "msg", 3)){
-		strtok_r(l->buf + 5, " ", &tok);
-		if(*(tok+strlen(tok)+1))
-		*(tok+strlen(tok))=' ';
-		raw("privmsg %s :%s\r\n", l->buf + 5, tok);
-		printf("\x1b[35mprivmsg %s :%s\x1b[0m\r\n", l->buf + 5, tok);
-	}
-	else
-        if (l->buf[1] == '#') {
-            strcpy(cdef, l->buf + 2);
-            printf("\x1b[35mnew channel: #%s\x1b[0m\r\n", cdef);
-        } else {
-            raw("%s\r\n", l->buf + 1);
-            printf("\x1b[35m%s\x1b[0m\r\n", l->buf);
-        }
-        break;
+	if(!strncmp(l->buf + 1, "JOIN", 4)||!strncmp(l->buf + 1, "join", 4)){
+		if(!strchr(l->buf, '#')){
+			printf("\x1b[35m%s\x1b[0m\r\n", l->buf);
+			printf("\x1b[35mIllegal channel!\x1b[0m\r\n");
+		} else {
+	                chan = strchr(l->buf, '#');
+			chan ++;
+			strcpy(l->prompt, chan);
+			raw("join #%s\r\n", chan);
+			printf("\x1b[35m%s\x1b[0m\r\n", l->buf);
+			printf("\x1b[35mJoined #%s!\x1b[0m\r\n", chan);
+		}
+	} else
+	if(!strncmp(l->buf + 1, "PART", 4)||!strncmp(l->buf + 1, "part", 4)){
+		tok = strchr(l->buf, '#');
+		if(strlen(l->buf) == 5){
+        	    raw("part #%s\r\n", chan);
+		    printf("\x1b[35m%s\x1b[0m\r\n", l->buf);
+        	    printf("\x1b[35mLeft #%s!\r\n", chan);
+            	    printf("\x1b[35mYou need to use /join or /# to speak in a channel!\x1b[0m\r\n");
+            	    chan = NULL;
+            	    strcpy(l->prompt, "");
+		} else
+		if(tok){
+            	    raw("part %s\r\n", tok);
+            	    printf("\x1b[35m%s\x1b[0m\r\n", l->buf);
+            	    printf("\x1b[35mLeft %s!\r\n", tok);
+            	    printf("\x1b[35mYou need to use /join or /# to speak in a channel!\x1b[0m\r\n");
+            	    chan = NULL;
+            	    strcpy(l->prompt, "");
+		} else {
+		    int ok = 1;
+		    tok = l->buf + 5;
+		    while(*tok){
+			if(*tok!=' '){
+			    ok = 0;
+			    break;
+			}
+			tok ++;
+		    }
+		    if(ok){
+			chan = l->prompt;
+                	printf("\n\n\n%s\n\n\n", chan);
+                	raw("part #%s\r\n", chan);
+                	printf("\x1b[35m%s\x1b[0m\r\n", l->buf);
+                	printf("\x1b[35mLeft #%s!\r\n", chan);
+                	printf("\x1b[35mYou need to use /join or /# to speak in a channel!\x1b[0m\r\n");
+                	chan = NULL;
+                	strcpy(l->prompt, "");
+		    } else {
+			printf("\x1b[35m%s\x1b[0m\r\n", l->buf);
+			printf("\x1b[35mIllegal channel!\x1b[0m\r\n");
+		    }
+		}
+	} else
     case '@':			/* send private message to target channel or user */
         strtok_r(l->buf, " ", &tok);
         if (l->buf[1] == '@') {
