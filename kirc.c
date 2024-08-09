365018f10b8b475958eb56d9b8b810e8b98dedb9
stefan11111 <stefan11111@shitposting.expert>
2024-08-09 17:46:28 +0300

    merge slot_process and slot_process_write into one function

--- kirc.c
+++ kirc.c
@@ -1626,91 +1626,34 @@ static inline void version(void)
     exit(0);
 }
 
-static void slot_process_write(state l, char *buf, size_t buf_len, size_t i) {
-    const char *err_str;
-    int sock_fd = dcc_sessions.sock_fds[i].fd;
-    int file_fd = dcc_sessions.slots[i].file_fd;
-
-    int n = read(file_fd, buf, buf_len);
-    if (n == -1) {
-        err_str = "read";
-        goto handle_err;
-    }
-
-    dcc_sessions.slots[i].bytes_read += n;
-    if (write(sock_fd, buf, n) < 0) {
-        err_str = "write";
-        goto handle_err;
-    }
-
-    size_t file_size = dcc_sessions.slots[i].file_size;
-    size_t bytes_read = dcc_sessions.slots[i].bytes_read;
-    unsigned ack_is_64 = file_size > UINT_MAX;
-    unsigned long long ack;
-    if (read(sock_fd, &ack, ack_is_64 ? 8 : 4) < 0) {
-        err_str = "read";
-        goto handle_err;
-    }
-
-    refresh_line(l);
-
-    if (bytes_read == file_size) {
-        goto close_fd;
-    }
-    return;
-handle_err:
-    if (errno == EAGAIN || errno == EWOULDBLOCK) {
-        return;
-    }
-    if (errno == ECONNRESET) {
-        goto close_fd;
-    }
-    perror(err_str);
-    dcc_sessions.slots[i].err_cnt++;
-    if (dcc_sessions.slots[i].err_cnt > ERR_MAX) {
-        goto close_fd;
-    }
-    return;
-close_fd:
-    shutdown(sock_fd, SHUT_RDWR);
-    shutdown(dcc_sessions.slots[i].write - 1, SHUT_RDWR);
-    close(dcc_sessions.slots[i].write - 1);
-    close(sock_fd);
-    close(file_fd);
-    slot_clear(i);
-}
-
 static void slot_process(state l, char *buf, size_t buf_len, size_t i) {
-    if (dcc_sessions.slots[i].write) {
-        slot_process_write(l, buf, buf_len, i);
-        return;
-    }
+    const int _write = dcc_sessions.slots[i].write;
     const char *err_str;
 
-    int sock_fd = dcc_sessions.sock_fds[i].fd;
-    int file_fd = dcc_sessions.slots[i].file_fd;
+    const int sock_fd = dcc_sessions.sock_fds[i].fd;
+    const int file_fd = dcc_sessions.slots[i].file_fd;
 
-    if (!(dcc_sessions.sock_fds[i].revents & POLLIN)) {
+    if (!(dcc_sessions.sock_fds[i].revents & POLLIN) && !_write) {
         return;
     }
 
-    int n = read(sock_fd, buf, buf_len);
+    int n = read(_write ? file_fd : sock_fd, buf, buf_len);
     if (n == -1) {
         err_str = "read";
         goto handle_err;
     }
 
-    if (n == 0) { /* EOF */
+    if ((n == 0) && !_write) { /* EOF */
         goto close_fd;
     }
 
     dcc_sessions.slots[i].bytes_read += n;
-    if (write(file_fd, buf, n) < 0) {
+    if (write(_write ? sock_fd : file_fd, buf, n) < 0) {
         err_str = "write";
         goto handle_err;
     }
 
-    if (!(dcc_sessions.sock_fds[i].revents & POLLOUT)) {
+    if (!(dcc_sessions.sock_fds[i].revents & POLLOUT) && !_write) {
         refresh_line(l);
         return;
     }
@@ -1719,8 +1662,9 @@ static void slot_process(state l, char *buf, size_t buf_len, size_t i) {
     unsigned ack_is_64 = file_size > UINT_MAX;
     unsigned ack_shift = (1 - ack_is_64) * 32;
     unsigned long long ack = htonll(bytes_read << ack_shift);
-    if (write(sock_fd, &ack, ack_is_64 ? 8 : 4) < 0) {
-        err_str = "write";
+
+    if ((_write ? read(sock_fd, &ack, ack_is_64 ? 8 : 4) : write(sock_fd, &ack, ack_is_64 ? 8 : 4))< 0) {
+        err_str = _write ? "read" : "write";
         goto handle_err;
     }
 
@@ -1745,6 +1689,10 @@ handle_err:
     return;
 close_fd:
     shutdown(sock_fd, SHUT_RDWR);
+    if (_write) {
+        shutdown(_write - 1, SHUT_RDWR);
+        close(_write - 1);
+    }
     close(sock_fd);
     close(file_fd);
     slot_clear(i);
