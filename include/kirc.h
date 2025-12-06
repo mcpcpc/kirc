9bdb1166910d418c3e47dbcc89f3f0436868039d
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2025-12-06 17:07:42 -0500

    update

--- include/kirc.h
+++ include/kirc.h
@@ -6,10 +6,11 @@
 #endif
 
 #include <fcntl.h>
-#include <sys/ioctl.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <limits.h>
 #include <netdb.h>
