5a4fa23ee5137e7421efa15944d847a49d6508ac
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2025-12-05 20:53:51 -0500

    update

--- src/editor.c
+++ src/editor.c
@@ -259,7 +259,7 @@ static int edit_insert(editor_context_t *ectx, const char *c)
     } else {
         /* Insert in the middle. */
         buffer_position_move_end(ectx, (ssize_t)clenb, 0);
-        memmove(ectx>buf + ectx->posb, c, clenb);
+        memmove(ectx->buf + ectx->posb, c, clenb);
         ectx->posu8++;
         ectx->lenu8++;
         ectx->posb += clenb;
