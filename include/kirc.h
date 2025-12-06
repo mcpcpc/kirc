2bb68c34cce5fc1ddbfdaade05dec088031e382c
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2025-12-06 00:24:47 -0500

    update

--- include/kirc.h
+++ include/kirc.h
@@ -9,11 +9,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
-#include <netdb.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <stdarg.h>
 #include <termios.h>
 #include <unistd.h>
+#include <netdb.h>
 #include <poll.h>
 #include <errno.h>
 #include <fcntl.h>
