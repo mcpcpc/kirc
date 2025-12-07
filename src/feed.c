20ea1039e9fdb0e3ba28ffa7da552f53ad798528
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2025-12-07 16:23:39 -0500

    update

--- src/feed.c
+++ src/feed.c
@@ -30,14 +30,12 @@ static void feed_privmsg(event_t *event)
 
 static void feed_join(event_t *event)
 {
-    printf("--> %s [%s]", event->nickname,
-        event->channel);
+    printf("--> %s", event->nickname);
 }
 
 static void feed_part(event_t *event)
 {
-    printf("<-- %s [%s]", event->nickname,
-        event->channel);
+    printf("<-- %s", event->nickname);
 }
 
 static void feed_quit(event_t *event)
