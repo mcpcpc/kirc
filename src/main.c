31855daa0ca3974dddbef49c823b780f3bc60e24
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2025-12-06 23:53:34 -0500

    update

--- src/main.c
+++ src/main.c
@@ -154,7 +154,7 @@ static int kirc_run(kirc_t *ctx)
 
                     event_t event;
                     event_init(&event, msg);
-                    print("worked!\n");
+                    printf("worked!\n");
 
                     if (event.type == EVENT_PING) {
                         network_send(ctx, "PONG\r\n");
