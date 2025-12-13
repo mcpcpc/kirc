870fdc215d17f8e99281acd2e92514641c8481c2
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2025-12-12 21:40:38 -0500

    update

--- src/editor.c
+++ src/editor.c
@@ -181,7 +181,7 @@ int editor_render(editor_t *editor)
     int offset = cols - 1;
 
     printf("\r:%.*s\x1b[0K", offset, editor->scratch[idx]);
-    printf("\x1b[%dC", editor->scratch_cursor + 1);
+    printf("\r\x1b[%dC", editor->scratch_cursor + 1);
 
     fflush(stdout);
 
