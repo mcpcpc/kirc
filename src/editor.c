957fedde9abd98f4d40238d6bee7bf76a8bea2ee
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2025-12-13 15:28:48 -0500

    update

--- src/editor.c
+++ src/editor.c
@@ -202,13 +202,13 @@ int editor_render(editor_t *editor)
 {
     int cols = terminal_columns(editor->ctx);
     //int limit = cols - 1;
-    int limit = cols - 8;
+    int limit = cols - 9;
     int len = strlen(editor->scratch);
 
     //printf("\r~%.*s\x1b[0K", limit, editor->scratch);
-    printf("\r%.*s\x1b[0K %03d/%03d",
+    printf("\r:%.*s\x1b[0K %03d/%03d",
         limit, editor->scratch, editor->cursor, len);
-    printf("\r\x1b[%dC", editor->cursor);
+    printf("\r\x1b[%dC", editor->cursor + 1);
 
     fflush(stdout);
 
