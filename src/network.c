d8b1061946b890bd708f519e90280d2d7d839ad8
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2025-12-26 22:05:15 -0500

    update

--- src/network.c
+++ src/network.c
@@ -125,7 +125,7 @@ int network_command_handler(network_t *network, char *msg)
                     printf("\rto " BOLD "%s" RESET ": * %s" CLEAR_LINE "\r\n",
                         network->ctx->target, text);
                     */
-                    printf("\rto " BOLD "%s" RESET ": \u007 %s" CLEAR_LINE "\r\n",
+                    printf("\rto " BOLD "%s" RESET ": \u2022 %s" CLEAR_LINE "\r\n",
                         network->ctx->target, text);
                 } else {
                     const char *err = "error: no channel set";
