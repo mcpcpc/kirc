bb00dd458efd0965da10d93b798f01d733b5d5a1
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2025-12-30 20:46:59 +0000

    add docstrings

--- include/base64.h
+++ include/base64.h
@@ -12,7 +12,10 @@
 
 const char base64_table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
+/* Calculate the size of the encoded data based on input length */
 size_t base64_encoded_size(size_t len);
+
+/* Encode input data to base64 format */
 int base64_encode(const uint8_t *in, size_t in_len, char *out, size_t out_size);
 
 #endif // __KIRC_BASE64_H
